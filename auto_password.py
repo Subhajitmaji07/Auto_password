281ffe5eddee3ac111217d85a2731a28451f588b
Subhajit <subhajitmaji575@gmail.com>
2025-07-02 08:16:15 +0530

    Initial commit

---
+++ auto_password.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+pass_len=int(input("Enter the length of the password: "))
+#creating the password
+lower=string.ascii_lowercase
+upper=string.ascii_uppercase
+num=string.digits
+symbol=string.punctuation
+
+
+all= lower+upper+num+symbol
+
+temp=random.sample(all,pass_len)
+password="".join(temp)
+print(password)
+
+#save password in text file
+save=input("Save password to file?(y/n): ")
+
+if save=="y":
+    web=input("Enter the website name: ")
+    
+    with open("password.txt","a") as f:
+        f.write(str(web)+"\t\t\t")
+        f.write(str(password))
+        f.write("\n")
+        print("password save to 'password.txt'file") 
